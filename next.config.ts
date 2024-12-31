069206c586adb053549a31ec4a09a3c56e9327e4
Rodrigo Yokota <ryok90@gmail.com>
2024-12-31 13:07:07 -0300

    feat: start main

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  }
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,18 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      josefin: 'Josefin Sans',
+      jomolhari: 'Jomolhari',
+    },
     extend: {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        'custom-black': '#1F1F1F',
+        'custom-salmon': '#EC6256',
+        'custom-gray': '#DFDFDF',
+        'custom-bg': '#EDEDED',
       },
     },
   },
